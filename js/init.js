2241cc620ba4d1a48e4db41ad330565fa93527ea
REFR9 <84752737+REFR9@users.noreply.github.com>
2021-06-25 17:50:11 -0500

    Create init.js

---
+++ js/init.js
@@ -0,0 +1,9 @@
+ firebase.initializeApp({
+ apiKey: "AIzaSyCbGcd069SCPUZG2x9Dc6J5JIm83cQJrzM",
+    authDomain: "refr9-cd1c6.firebaseapp.com",
+    projectId: "refr9-cd1c6",
+    storageBucket: "refr9-cd1c6.appspot.com",
+    messagingSenderId: "387834850886",
+    appId: "1:387834850886:web:538223ff4d10ae58f5e447",
+    measurementId: "G-SQ9YG0RHTG"
+   });
